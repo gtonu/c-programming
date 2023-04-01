7367c4b5f6a6f289dbf468691193ea503f722d3e
gtonu <sanzidtonu@gmail.com>
2023-04-02 02:29:02 +0600

    hello.c

--- setup/hello.c
+++ setup/hello.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int a=15;
-     int b=15;
-      printf("Total is: %d\n",a+b);
+    int a;
+     int b;
+     scanf("%d %d",&a,&b);
+      printf("%d\n",a+b);
      return 0;
 }
